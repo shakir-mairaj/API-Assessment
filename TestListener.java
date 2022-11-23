9488486ccad918c800d050451d7f56cfc39bfa7e
shakir-mairaj <shakir.mufti@gmail.com>
2022-11-23 13:45:49 +0530

    Add files via upload

---
+++ TestListener.java
@@ -0,0 +1,65 @@
+/*      Copyright 2022 Mohammad Faisal Khatri
+
+        Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.
+*/
+
+package io.github.mfaisalkhatri.utility;
+
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.ITestContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import static org.apache.commons.lang3.StringUtils.repeat;
+
+/**
+ * Created By Faisal Khatri on 19-11-2021
+ */
+
+public class TestListener implements ITestListener {
+
+    private static final Logger LOGGER = LogManager.getLogger (TestListener.class);
+
+    private void logMessage (final String message) {
+        this.LOGGER.info ("\n");
+        this.LOGGER.info (repeat ("=", 75));
+        this.LOGGER.info (message);
+        this.LOGGER.info (repeat ("=", 75));
+        this.LOGGER.info ("\n");
+    }
+
+    @Override
+    public void onTestStart (final ITestResult result) {
+        logMessage ("Test Execution Started...." + result.getName ());
+    }
+
+    @Override
+    public void onTestSuccess (final ITestResult result) {
+        logMessage ("Test Passed Successfully." + result.getName ());
+
+    }
+
+    @Override
+    public void onTestFailure (final ITestResult result) {
+        logMessage ("Test Failed!!!!" + result.getName ());
+    }
+
+    @Override
+    public void onFinish (final ITestContext context) {
+        logMessage ("Test Execution Completed Successfully for all tests!!" + context.getSuite ()
+            .getAllMethods ());
+
+    }
+
+}
